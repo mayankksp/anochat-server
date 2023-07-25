e93e1f1f8cf8d144733e8a0f681da18f99df5495
Mayank Kashyap <contact@mayankkashyap.in>
2023-07-25 19:20:57 +0530

    fix a login bug

--- controllers/api/v1/userController.js
+++ controllers/api/v1/userController.js
@@ -1,6 +1,7 @@
 // anochat-server/controllers/api/v1/userController.js
 const User = require('../../../models/User');
 const generateToken = require('../../../utils/jwt');
+const bcrypt = require('bcryptjs');
 
 exports.register = async (req, res) => {
   const { username, password } = req.body;
@@ -17,12 +18,52 @@ exports.register = async (req, res) => {
 
     await user.save();
 
-    const token = await generateToken(user.id);
-
-    res.json({ token });
+    res.json({ msg: 'User registered successfully' });
 
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
   }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    let user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    const token = await generateToken(user.id);
+
+    // remove sensitive data before sending user object
+    user.password = undefined;
+
+    // include the user object in the response
+    res.json({ token, user });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+exports.getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+exports.logout = (req, res) => {
+  // Invalidate the token
+  res.json({ msg: "Logged out" });
 };
--- routes/api/v1/userRoutes.js
+++ routes/api/v1/userRoutes.js
@@ -1,9 +1,13 @@
 // anochat-server/routes/api/v1/userRoutes.js
 const express = require('express');
-const { register } = require('../../../controllers/api/v1/userController');
+const { register, login, getProfile, logout } = require('../../../controllers/api/v1/userController');
+const auth = require('../../../middleware/auth'); // You'll need to create this
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/login', login);
+router.get('/me', auth, getProfile);  // This route should be protected
+router.post('/logout', auth, logout);  // This route should be protected
 
 module.exports = router;
